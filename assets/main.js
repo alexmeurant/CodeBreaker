f4523fd3c8b204779684c1a5bcc8994d017569ef
MEURANT <alexandremeurant@hotmail.fr>
2017-09-17 15:43:11 +0200

    fix

--- assets/main.js
+++ assets/main.js
@@ -1,115 +1,74 @@
-let answer = document.getElementById('answer').value;
-let divHTML = "";
-let attempt = document.getElementById('attempt').value;
-let results = document.getElementById('results');
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/main.css" />
+    <title>Code Breaker</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="col-md-6">
+        <h1>Code Breaker</h1>
+        <div class="row">
+          <p id="code" class="code">
+            <strong>????</strong>
+          </p>
+        </div>
 
- // This function generates a random number with exactly 4 digits :
- function setHiddenFields() {
-     answer = Math.floor(Math.random() * 10000).toString();
-     if (answer.length !== 4) {
-         while (answer.length < 4) {
-             answer = "0" + answer;
-         }
-     }
-     return answer;
- }
+        <div id="guessing-div" class="row form-inline">
+          <input type="hidden" id="attempt"/>
+          <input type="hidden" id="answer"/>
+          <input id="user-guess" class="form-control" type="number" />
+          <button class="btn btn-primary" onclick="guess()">Submit Guess</button>
+        </div>
 
-// This function displays a custom message when called :
- function setMessage(text) {
-     let message = document.getElementById('message');
-     message.innerHTML = text;
- }
+        <div id="replay-div" class="row" style="display:none">
+          <button class="btn btn-primary" onclick="window.location.reload()">Play again?</button>
+        </div>
 
- // This function checked if the guessed number has 4 digits :
- function validateInput(guessNumber) {
-     if (guessNumber.length === 4) {
-         return true;
-     } else {
-         setMessage("Guesses must be exactly 4 characters long.");
-         return false;
-     }
- }
+        <div class="row">
+          <p id="message" class="message"></p>
+        </div>
 
- // This function get the results and indications about the digit position :
- function getResults(guessNumber) {
-     let correctCharacter = 0;
-     divHTML = '<div class="row"><span class="col-md-6">' + guessNumber + '</span><div class="col-md-6">';
+        <div class="row" id="results">
+          <div class="row">
+            <strong class="col-md-6">Guess</strong>
+            <strong class="col-md-6">Result</strong>
+          </div>
+        </div>
+      </div>
 
-     for (var i = 0; i < guessNumber.length; i++) {
-         let guessCharacter = guessNumber.charAt(i);
-         if (guessCharacter === answer.charAt(i)) {
-             divHTML += '<span class="glyphicon glyphicon-ok"></span>';
-             correctCharacter += 1;
-         } else if (answer.indexOf(guessCharacter) > -1) {
-             divHTML += '<span class="glyphicon glyphicon-transfer"></span>';
-         } else {
-             divHTML += '<span class="glyphicon glyphicon-remove"></span>';
-         }
-     }
-     divHTML += '</div></div>';
+      <div class="col-md-6">
+        <h2>Objective:</h2>
+        <p class="lead">Guess the randomly generated 4 digit code.</p>
 
-
-     if (correctCharacter === 4) {
-         return true;
-     } else {
-         return false;
-     }
- }
-
-// This is the main function that called all other functions :
- function guess() {
-
-     let input = document.getElementById('user-guess').value;
-
-     if (answer === "") {
-         setHiddenFields();
-     }
-     if (attempt === "") {
-         attempt = 0;
-     }
-
-     validateInput(input);
-
-     if (!validateInput(input)) {
-         return false;
-     } else {
-         attempt += 1;
-     }
-
-     getResults(input);
-     results.innerHTML += divHTML;
-
-     // Message and results are shown depending on the situation :
-     if (getResults(input)) {
-         setMessage("You Win! :)");
-         showAnswer(getResults(input));
-         showReplay();
-     } else if (!getResults(input) && attempt < 10) {
-         setMessage("Incorrect, try again.");
-     } else if (attempt >= 10) {
-         setMessage("You Lose! :(");
-         showAnswer(getResults(input));
-         showReplay();
-     } 
- }
-
-// This function shows the random code to be guessed :
- function showAnswer(isTrue) {
-     let code = document.getElementById('code');
-     code.innerHTML = answer;
-     if (isTrue) {
-         code.className += " success";
-     } else {
-         code.className += " failure";
-     }
- }
-
- // This function gives the user the opportunity to play again :
- function showReplay() {
-     // the guessing block is hidden:
-     let hideGuessingDiv = document.getElementById('guessing-div');
-     hideGuessingDiv.style.display = "none";
-     // the repaly block is displayed:
-     let showReplayDiv = document.getElementById('replay-div');
-     showReplayDiv.style.display = "block";
- }
+        <h2>Rules:</h2>
+        <ul>
+          <li>Each guess must consist of 4 numeric characters.</li>
+          <li>Numbers may be used more than once!</li>
+          <li>You win only if your guess is an exact match.</li>
+          <li>You lose if you fail to guess the code under 10 guesses.</li>
+          <li>
+            <span class="glyphicon glyphicon-ok"></span>
+            Indicates a number is in the correct place.
+          <li>
+            <span class="glyphicon glyphicon-transfer"></span>
+            Indicates a number is part of the code, but not in the right position.
+          </li>
+          <li>
+            <span class="glyphicon glyphicon-transfer"></span>
+            Doesn't consider how many times a number exists in the code.
+          </li>
+          <li>
+            <span class="glyphicon glyphicon-remove"></span>
+            Indicates a number is not part of the code.
+          </li>
+        </ul>
+      </div>
+    </div>
+    <script src="assets/main.js"></script>
+  </body>
+</html>
